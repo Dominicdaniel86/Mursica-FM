46ee1f513fe3e05cb5539f53a96460336a3eab42
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-02-05 11:34:00 +0100

    Add basic fetch to frontend to call the backend API

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+async function fetchData() {
+    try {
+        const response = await fetch('http://localhost:80/api');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.text();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data: ', error);
+    }
+}
+
+fetchData();
