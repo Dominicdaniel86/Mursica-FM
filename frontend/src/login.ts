e5965ef9e03acffbcc9fd74ee0ac99331f1b57fe
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-02-09 02:18:57 +0100

    Add links and routing logic to login and registration pages

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,3 +1,18 @@
-function login() {
+window.addEventListener('load', () => {
+    console.log('DOM has loaded!');
 
+    // Reset username
+    let usernameInputElement: HTMLInputElement = document.getElementById('username-input') as HTMLInputElement;
+    usernameInputElement.value = '';
+});
+
+function login() {
+    let usernameInputElement: HTMLInputElement = document.getElementById('username-input') as HTMLInputElement;
+    let usernameInput: string = usernameInputElement.value;
+
+    if(usernameInput.length === 0) {
+        alert('Invalid credentials!');
+    } else {
+        window.location.href = '/static/html/add-song.html';
+    }
 }
