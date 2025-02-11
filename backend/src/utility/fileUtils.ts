e637eae90093eda7af49baa778ed90272626f055
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-02-11 21:44:08 +0100

    Switch writes to env file from asynchronous to synchronous

--- backend/src/utility/fileUtils.ts
+++ backend/src/utility/fileUtils.ts
@@ -2,35 +2,25 @@ import * as fs from 'fs';
 import logger from '../logger.js';
 
 export function writeToEnvFile(key: string, value: string) {
-    fs.readFile('./.env', 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
-        if (err) {
-            logger.error('Failed to read .env file: ', err);
-            return;
+
+    let data = fs.readFileSync('./.env', 'utf8');
+
+    let envValues: Array<string> = data.split('\n').filter(line => line.trim());
+    let tokenUpdated = false;
+
+    envValues = envValues.map(line => {
+        if (line.startsWith(key)) {
+            tokenUpdated = true;
+            return `${key}=${value}`;
         }
-
-        let envValues: Array<string> = data.split('\n').filter(line => line.trim());
-        
-        let tokenUpdated = false;
-        envValues = envValues.map(line => {
-            if (line.startsWith(key)) {
-                tokenUpdated = true;
-                return `${key}=${value}`;
-            }
-            return line;
-        });
-
-        if(!tokenUpdated) {
-            envValues.push(`${key}=${value}`);
-        }
-
-        const updatedContent = envValues.join('\n') + '\n';
-
-        fs.writeFile('./.env', updatedContent, err => {
-            if(err)
-                logger.error(`Failed to write ${key}:${value} into .env file: `, err);
-            else
-                logger.info(`Successfully updated .env with ${key}:${value}`);
-        });
-
+        return line;
     });
+
+    if(!tokenUpdated) {
+        envValues.push(`${key}=${value}`);
+    }
+
+    const updatedContent = envValues.join('\n') + '\n';
+    fs.writeFileSync('./.env', updatedContent, 'utf-8');
+    logger.info(`Successfully updated .env with ${key}:${value}`);
 }
