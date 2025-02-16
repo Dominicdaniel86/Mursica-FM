f6c5b75a3ef800ebc5cd3742543076f63401da92
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-02-16 22:22:48 +0100

    Fetch searchSong api in the frontend

--- frontend/src/add-song.ts
+++ frontend/src/add-song.ts
@@ -1,3 +1,40 @@
-function switchToAdmin() {
+import { TrackSummary } from "./interfaces/search-song";
+
+declare global {
+    interface Window {
+        switchToAdmin: () => void;
+    }
+}
+
+window.addEventListener('load', () => {
+    console.log('DOM has loaded');
+
+    // Reset searched song value
+    let searchSongElement: HTMLInputElement = document.getElementById('search-song') as HTMLInputElement;
+    searchSongElement.value = '';
+});
+
+export function switchToAdmin() {
     window.location.href = '/static/html/admin.html';
 }
+
+async function searchSong(input: string) {
+
+    const url: string = '/api/tracks/search/?trackTitle=Master of Puppets';
+
+    let response = await axios.get<TrackSummary>(url);
+
+    console.log(response.data);
+}
+
+let searchSongElement: HTMLInputElement = document.getElementById('search-song') as HTMLInputElement;
+let timeoutID: number;
+
+searchSongElement.addEventListener('input', () => {
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => {
+        searchSong(searchSongElement.value);
+    }, 1750);
+});
+
+window.switchToAdmin = switchToAdmin;
---
+++ frontend/src/interfaces/search-song.ts
@@ -0,0 +1,6 @@
+// Filtered track search response
+export interface TrackSummary {
+    id: string;
+    title: string;
+    albumImage: string;
+}
