12d39627bd39be78c456d4f72ff9df558b73803f
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-03-26 22:55:39 +0100

    Remove some deprecated values in index.ts and the new .example.env

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,6 +1,5 @@
 export * from './auth/clientCredentials.js'
 export * from './auth/OAuth.js';
 export * from './auth/logout.js';
-export * from './searchSong.js'
 export * from './trackInfo.js';
 export * from './adminControl.js';
