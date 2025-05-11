855050839023c48e8c65e98a9aced0df4bc6f8a8
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-05-11 12:21:31 +0200

    OLP-24 Adjust githook activation script

--- scripts/activate-githooks.sh
+++ scripts/activate-githooks.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Call the deactivate-githook.sh script if it exists
+DEACTIVATE_SCRIPT="./scripts/deactivate-githooks.sh"
+
+if [ -f "$DEACTIVATE_SCRIPT" ]; then
+    echo "Running deactivate-githooks.sh..."
+    bash "$DEACTIVATE_SCRIPT"
+else
+    echo "No deactivate-githooks.sh script found. Skipping."
+fi
+
 # Define the source and destination directories
 GITHOOKS_DIR=".githooks"
 GIT_HOOKS_DEST=".git/hooks"
@@ -18,6 +28,11 @@ fi
 
 # Copy all files from .githooks to .git/hooks
 cp -a "$GITHOOKS_DIR/." "$GIT_HOOKS_DEST/"
+
+# Rename all hook files to remove the .sh extension
+for hook in "$GIT_HOOKS_DEST/"*.sh; do
+    mv "$hook" "${hook%.sh}"
+done
 
 # Make sure all hook scripts are executable
 chmod +x "$GIT_HOOKS_DEST/"*
