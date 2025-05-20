35d91a051d48b398e4d3d8a46e84e20a5c77a8eb
Dominic Daniel Krämer <Dominicdaniel3107@gmail.com>
2025-05-20 13:00:45 +0200

    OLP-08 feat OLP-08 feat Add placeholder functions for algorithm based backend calculations

---
+++ backend/src/services/algorithm.ts
@@ -0,0 +1,5 @@
+export async function getNextTrack(): Promise<void> {}
+
+export async function generatePlaylist(): Promise<void> {}
+
+export async function getWishlist(): Promise<void> {}
---
+++ backend/src/services/trackManagement.ts
@@ -0,0 +1,1 @@
+export async function addTrackToWishlist(): Promise<void> {}
